76eac510dde275350bc1d569406f0102c4123b6e
herpereyra <hernanrodrpereyra@gmail.com>
2023-05-15 11:30:55 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
